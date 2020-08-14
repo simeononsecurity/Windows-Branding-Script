102e0c0918d1201001894b9539a6e927f990c125
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-08-14 00:26:11 -0700

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Setup branding on Windows 10 and Server 2016/2019 Systems
+
+## How to set up the branding files
+
+- [X] Replace all images with your branding images
+ - [X] OEM logo must be either 95x95 or 120x20 and in ".bmp" format
+ - [X] Generate the User Image along with 32x32, 40x40, 48x48, 192x192 variants.
+ - [X] Generate or copy user image for guest image.
+
+## How to run the script
+```
+.\sos-copybranding.ps1
+```
---
+++ sos-copybranding.ps1
@@ -0,0 +1,21 @@
+if (test-path C:\temp\Files\Branding){
+Write-host branding files already moved
+}else {
+mkdir C:\temp\Files\Branding
+copy-item -Path .\Files\Files\Branding\* -Destination C:\temp\Files\Branding\ -Recurse -Force
+}
+copy-item -Path .\Files\Branding\wallpaper.jpg -Destination C:\Windows\Web\Screen\lockscreen.jpg -Force
+copy-item -Path .\Files\Branding\wallpaper.jpg -Destination C:\Windows\Web\Wallpaper\Theme1\wallpaper.jpg -Force
+copy-item -Path .\Files\Branding\oemlogo.bmp -Destination "C:\ProgramData\Microsoft\User Account Pictures" -Force
+copy-item -Path .\Files\Branding\user*.png -Destination "C:\ProgramData\Microsoft\User Account Pictures" -Force
+copy-item -Path .\Files\Branding\user*.bmp -Destination "C:\ProgramData\Microsoft\User Account Pictures" -Force
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name Wallpaper -Type String -Value C:\Windows\Web\Wallpaper\Theme1\wallpaper.jpg -Force
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" -Name Logo -Type String -Value "C:\ProgramData\Microsoft\User Account Pictures\oemlogo.bmp" -Force
+##Enable Dark Mode
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name AppsUseLightTheme -Type DWORD -Value "00000000" -Force
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name SystemUsesLightTheme -Type DWORD -Value "00000000" -Force
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name ColorPrevalence -Type DWORD -Value "00000000" -Force
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name EnableTransparency -Type DWORD -Value "00000001" -Force
+
+#Branding GPOs
+.\Files\LGPO\LGPO.exe /g .\Files\GPO
